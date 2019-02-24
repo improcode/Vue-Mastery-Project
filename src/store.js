f77a4676b909c6dab2fb03b12a8b8d501c700085
improcode <art.milkowski@gmail.com>
2019-02-24 13:40:04 +0100

    Mutation and Action part 2

--- src/store.js
+++ src/store.js
@@ -33,21 +33,24 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    fetchEvents({commit}, {perPage, page}) {
-      EventService.getEvents(perPage, page)
-      .then(response => {
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page).then(response => {
         commit('SET_EVENTS', response)
       })
     },
-    fetchEvent({commit}, id) {
-      EventService.getEvent(id)
-      .then(response => {
-        commit('SET_EVENT', response.data)
-        console.log(this.event)
-      })
-      .catch(error => {
-        console.log('There was an error' + error)
-      })
+    fetchEvent({ commit, getters }, id) {
+      var event = getters.getEventById(id)
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            console.log('There was an error' + error)
+          })
+      }
     },
     createEvent({ commit }, event) {
       return EventService.postEvent(event).then(() => {
@@ -57,7 +60,7 @@ export default new Vuex.Store({
   },
   getters: {
     getEventById: state => id => {
-      return state.events.find(event => event.id === id)
+      state.event = state.events.find(event => event.id === id)
     },
     catLength: state => {
       return state.categories.length
